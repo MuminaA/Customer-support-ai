018ede8fd8e440a76653d4328e95bca76552753d
RK <113088626+razveen63@users.noreply.github.com>
2024-08-11 02:08:35 +0400

    Frontend and 1st function
    Using material UI to develop the frontend of the API and added the sendMessage function (partially)

--- app/page.js
+++ app/page.js
@@ -1,16 +1,92 @@
-// because using states
 'use client'
 import Image from "next/image";
 import { useState } from "react";
-import { Box } from '@mui/material'
+import { Box, Stack, TextField, Button } from '@mui/material'
 
 export default function Home() {
-  const [messages, setMessages] = useState({
-    role: `assistant`,
-    content: `Hi I'm the Headstarter Support Agent, how can i assist you today?`,
-  })
+  const [messages, setMessages] = useState([
+    {
+      role: `assistant`,
+      content: `Hi I'm the Headstarter Support Agent, how can I assist you today?`,
+    }
+  ]);
 
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('');
 
-  return <Box></Box>
+  const sendMessage = async()=>{
+    setMessage('')
+    setMessages((messages)=>[
+      ...messages,
+      {role: 'user', content:'message'},
+      {role: 'assistant', content: ''},
+    ])
+    const response = fetch('api/chat', {
+      method: "POST",
+      headers:{
+        'Content-Type' : 'aplication/json'
+      }
+    })
+  }
+
+  return (
+    <Box 
+      width="100vw" 
+      height="100vh" 
+      display="flex" 
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        direction="column"
+        width="600px"
+        height="700px"
+        border="1px solid black"
+        p={2}
+        spacing={3}
+      >
+        <Stack
+          direction="column"
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          maxHeight="100%"
+        >
+          {
+            messages.map((message, index) => (
+              <Box 
+                key={index} 
+                display="flex"
+                justifyContent={message.role === 'assistant' 
+                  ? 'flex-start' 
+                  : 'flex-end'}
+              >
+                <Box 
+                  bgcolor={message.role === 'assistant' 
+                    ? 'primary.main' 
+                    : 'secondary.main'}
+                  color='white'
+                  borderRadius={16}
+                  p={3}
+                >
+                  {message.content}
+                </Box>
+              </Box>
+            ))
+          }
+          
+        </Stack>
+        <Stack direction={"row"} spacing={2}>
+          <TextField
+          label = "Type your message here"
+          fullWidth
+          value = {message}
+          onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant = "Contained">Send</Button>
+
+        </Stack>
+      </Stack>
+    </Box>
+  );
 }
