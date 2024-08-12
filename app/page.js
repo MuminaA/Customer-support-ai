2bd5a8dde8eddb3ed06e2d3a36b146f86868bc36
mina <muminaabdi13@gmail.com>
2024-08-11 22:20:13 -0400

    better front-end design created

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
 "use client";
 // import Image from "next/image";
 import { useState, useRef, useEffect } from "react";
-import { Box, Stack, TextField, Button } from "@mui/material";
+import { Box, Stack, TextField, Button, Typography } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function Home() {
   const [messages, setMessages] = useState([
@@ -22,6 +27,48 @@ export default function Home() {
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: " #e6e6e6",
+    // "&:hover": {
+    //   backgroundColor: '#f2f2f2',
+    // },
+    // marginLeft: 0,
+    // width: "100%",
+    // [theme.breakpoints.up("sm")]: {
+    //   marginLeft: theme.spacing(1),
+    //   width: "auto",
+    // },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    width: "100%",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      // transition: theme.transitions.create("width"),
+      // [theme.breakpoints.up("sm")]: {
+      //   width: "12ch",
+      //   "&:focus": {
+      //     width: "20ch",
+      //   },
+      // },
+    },
+  }));
 
   const [message, setMessage] = useState("");
 
@@ -71,103 +118,195 @@ export default function Home() {
       width="100vw"
       height="100vh"
       display="flex"
-      flexDirection="column"
-      justifyContent="center"
+      // flexDirection="column"
+      // justifyContent="center"
       alignItems="center"
+      // backgroundColor="yellow"
     >
       <Stack
-        direction="column"
-        width="600px"
-        height="700px"
-        border="1px solid black"
-        p={2}
-        spacing={3}
-        sx={{ borderRadius: 6 }}
+        height="100vh"
+        width="250px"
+        border="1px solid #e6e6e6"
+        // backgroundColor="red"
+        display="flex"
+        flexDirection="column"
       >
         <Stack
-          direction="column"
-          spacing={2}
-          flexGrow={1}
-          overflow="auto"
-          maxHeight="100%"
+          height="40px"
+          display="flex"
+          alignItems="center"
+          flexDirection="row"
+          borderBottom="1px solid #e6e6e6"
+          gap={1}
+          padding="0 10px"
+          // backgroundColor="blue"
         >
-          {messages.map((message, index) => (
-            <Box
-              key={index}
-              display="flex"
-              justifyContent={
-                message.role === "assistant" ? "flex-start" : "flex-end"
-              }
-            >
-              <Box
-                // bgcolor={
-                //   message.role === "assistant"
-                //     ? "primary.main"
-                //     : "secondary.main"
-                // }
-                color="black"
-                borderRadius={16}
-                p={3}
-              >
-                <strong>
-                  {message.role === "assistant" ? "Bot: " : "You: "}
-                </strong>
-                {message.content}
-              </Box>
-            </Box>
-          ))}
-          <div ref={messagesEndRef} />
+          <SmartToyIcon />
+          <Typography fontWeight="bold">ChatAI</Typography>
         </Stack>
         <Stack
           display="flex"
-          justifyContent="center"
-          alignItems="center"
-          direction={"row"}
-          spacing={2}
-          sx={{
-            border: "1px solid #f2f2f2",
-            borderRadius: "50px",
-            paddingRight: "10px",
-            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-          }}
+          gap={1.5}
+          margin="15px 10px"
+          // backgroundColor="purple"
+          flexGrow={1}
         >
-          <TextField
-            placeholder="Type your prompt here..."
-            fullWidth
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                "& fieldset": {
-                  border: "none",
-                },
-              },
-            }}
-          />
           <Button
             variant="contained"
-            onClick={sendMessage}
-            disabled={message.trim() === ""}
             sx={{
-              backgroundColor: "#33cc33",
-              borderRadius: "50%", // Make the button circular
-              minWidth: "40px", // Adjust the width and height to make it a circle
-              height: "40px",
-              padding: 0,
+              backgroundColor: "#6666ff",
               display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
+              justifyContent: "flex-start",
+              padding: "8px 16px",
+              width: "100%",
               "&:hover": {
-                backgroundColor: "#2eb82e", // Keep the color the same on hover
-              },
-              "&:disabled": {
-                backgroundColor: "#cccccc", // Change color when disabled
-                cursor: "not-allowed",
+                backgroundColor: "#8080ff",
               },
             }}
           >
-            <SendIcon />
+            <Typography fontWeight="bold" marginRight="23px">
+              +
+            </Typography>{" "}
+            New chat
           </Button>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Stack>
+        <Stack
+          // backgroundColor="yellow"
+          height="60px"
+          borderTop="1px solid #e6e6e6"
+          display="flex"
+          flexDirection="row"
+          alignItems="center"
+          gap={2}
+          paddingLeft="10px"
+        >
+          <AccountCircleIcon fontSize="large" />
+          <Stack>
+            <Typography fontWeight="bold" fontSize="">
+              Victony Simmons
+            </Typography>
+            <Typography fontSize=".8rem" color='#a6a6a6'>Portal</Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+      <Stack
+        // width="100vh"
+        flex={1}
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="#f2f2f2"
+      >
+        <Stack
+          direction="column"
+          width="800px"
+          // height="700px"
+          // width="100vh"
+          height="100vh"
+          marginTop="15px"
+          // border="1px solid black"
+          p={2}
+          spacing={3}
+          sx={{ borderRadius: 6 }}
+          // backgroundColor="red"
+        >
+          <Stack
+            direction="column"
+            spacing={2}
+            flexGrow={1}
+            overflow="auto"
+            maxHeight="100%"
+          >
+            {messages.map((message, index) => (
+              <Box
+                key={index}
+                display="flex"
+                justifyContent={
+                  message.role === "assistant" ? "flex-start" : "flex-end"
+                }
+              >
+                <Box
+                  bgcolor={message.role === "assistant" ? "white" : "#ffe6cc"}
+                  color="black"
+                  borderRadius={3}
+                  p={3}
+                >
+                  <strong>
+                    {message.role === "assistant" ? "Bot: " : "You: "}
+                  </strong>
+                  {message.content}
+                </Box>
+              </Box>
+            ))}
+            <div ref={messagesEndRef} />
+          </Stack>
+          <Stack>
+            <Stack
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              direction={"row"}
+              spacing={2}
+              sx={{
+                border: "1px solid #f2f2f2",
+                borderRadius: "50px",
+                paddingRight: "10px",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+              }}
+            >
+              <TextField
+                placeholder="Type your prompt here..."
+                fullWidth
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      border: "none",
+                    },
+                  },
+                }}
+              />
+              <Button
+                variant="contained"
+                onClick={sendMessage}
+                disabled={message.trim() === ""}
+                sx={{
+                  backgroundColor: "#33cc33",
+                  borderRadius: "50%", // Make the button circular
+                  minWidth: "40px", // Adjust the width and height to make it a circle
+                  height: "40px",
+                  padding: 0,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  "&:hover": {
+                    backgroundColor: "#2eb82e", // Keep the color the same on hover
+                  },
+                  "&:disabled": {
+                    backgroundColor: "#cccccc", // Change color when disabled
+                    cursor: "not-allowed",
+                  },
+                }}
+              >
+                <SendIcon />
+              </Button>
+            </Stack>
+            <Typography display='flex' justifyContent='center' marginTop='20px' color='#bfbfbf'>
+              ChatAI has the potential to generate Incorrect Information.
+            </Typography>
+          </Stack>
         </Stack>
       </Stack>
     </Box>
